6922b840e205697561593beb26f92d1cda67bb14
AliArefi1993 <77581271+AliArefi1993@users.noreply.github.com>
2021-01-20 17:42:11 +0330

    Add files via upload

---
+++ TheAntsGoMarchingLyric.py
@@ -0,0 +1,26 @@
+# TheAntsGoMarchingLyric.py
+# This program print the lyrics of The Ants Go Marching 
+# Writen by: Ali Arefi
+
+def marchinglLyric(quantity, disturb):
+    print("The ants go marching {0} by {0}, hurrah, hurrah\n\
+    The ants go marching {0} by {0}, hurrah, hurrah\n\
+    The ants go marching {0} by {0},\n\
+    The little one stops to {1},\n\
+    And they all go marching down to the ground\n\
+    To get out of the rain, BOOM! BOOM! BOOM!\n\n" .format(quantity, disturb))
+
+def main():
+    marchinglLyric("one", "suck his thumb")
+    marchinglLyric("two", "tie his shoe")
+    marchinglLyric("three", "climb a tree")
+    marchinglLyric("four", "shut the door")
+    marchinglLyric("five", "take a dive")
+    marchinglLyric("six", "pick up sticks")
+    marchinglLyric("seven", "pray to heaven")
+    marchinglLyric("eight", "roller skate")
+    marchinglLyric("nine", "scheck the time")
+    marchinglLyric("ten", "shout 'The End'")
+
+
+main()
